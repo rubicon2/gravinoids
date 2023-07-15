f3452b4005b8b28332afa710a4d2b4811fd01555
Liam <liargent@hotmail.com>
2023-07-15 14:15:31 +0100

    Prettify input.js

--- src/modules/input.js
+++ src/modules/input.js
@@ -1,24 +1,23 @@
 // When a binding is created, keyboard code will be extracted and set here
 // corresponding value will be an array of the input sequences that are interested in that keycode
-import { loopInt } from "./util";
+import { loopInt } from './util';
 
 const slowInputTimeout = 750;
 const standardInputTimeout = 600;
 const fastInputTimeout = 500;
 
 class InputController {
-
     // Points the way for the key event to the correct binding group
-    static boundInputs = new Map(); 
+    static boundInputs = new Map();
     // E.g. each player has their own group
     static bindingGroups = new Map();
 
     static addBindingGroup(groupName, isActive, inputSequences) {
         if (!this.bindingGroups.has(groupName)) {
             this.bindingGroups.set(groupName, { isActive, inputSequences });
-            inputSequences.forEach(inputSeq => {
+            inputSequences.forEach((inputSeq) => {
                 let inputs = inputSeq.getAllInputs();
-                inputs.forEach(input => {
+                inputs.forEach((input) => {
                     if (!this.boundInputs.has(input)) {
                         this.boundInputs.set(input, groupName);
                     } else {
@@ -29,7 +28,7 @@ class InputController {
         } else {
             console.warn(`Attempted to overwrite binding group ${groupName}`);
         }
-    } 
+    }
 
     static createBoundInputKey(event) {
         return `${event.type} ${event.code}`;
@@ -37,12 +36,13 @@ class InputController {
 
     static #getAssociatedInputGroup(event) {
         let groupName = this.boundInputs.get(this.createBoundInputKey(event));
-        return this.bindingGroups.get(groupName);  
+        return this.bindingGroups.get(groupName);
     }
 
     static handleKeyDown(event) {
         if (this.boundInputs.has(this.createBoundInputKey(event))) {
-            for (let inputSequence of this.#getAssociatedInputGroup(event).inputSequences) {
+            for (let inputSequence of this.#getAssociatedInputGroup(event)
+                .inputSequences) {
                 inputSequence.handleInput(event);
             }
         }
@@ -50,16 +50,15 @@ class InputController {
 
     static handleKeyUp(event) {
         if (this.boundInputs.has(this.createBoundInputKey(event))) {
-            for (let inputSequence of this.#getAssociatedInputGroup(event).inputSequences) {
+            for (let inputSequence of this.#getAssociatedInputGroup(event)
+                .inputSequences) {
                 inputSequence.handleInput(event);
             }
         }
     }
-
 }
 
 class InputSequence {
-
     #inputStage = 0;
     #timeout = null;
 
@@ -75,19 +74,21 @@ class InputSequence {
         let nextInput = this.inputs[this.#inputStage];
         // If input matches expected next input in sequence
         if (nextInput.code === code) {
-            if (nextInput.action)
-                nextInput.action();
-            this.#inputStage = loopInt(0, this.inputs.length, this.#inputStage + 1);
+            if (nextInput.action) nextInput.action();
+            this.#inputStage = loopInt(
+                0,
+                this.inputs.length,
+                this.#inputStage + 1
+            );
             // If next input not detected before timeout triggers, sequence will be reset
             this.#setNewInputTimeout(nextInput.timeout);
-        // If user is pressing the first key in sequence more than once
+            // If user is pressing the first key in sequence more than once
         } else if (this.inputs[0].code === code) {
             let firstInput = this.inputs[0];
-            if (firstInput.action)
-                firstInput.action();
+            if (firstInput.action) firstInput.action();
             this.#inputStage = loopInt(0, this.inputs.length, 1);
             this.#setNewInputTimeout(firstInput.timeout);
-        // If event code does not match next expected input or first input, and inputStage is not zero, sequence must be broken, reset
+            // If event code does not match next expected input or first input, and inputStage is not zero, sequence must be broken, reset
         } else if (this.#inputStage != 0) {
             this.#handleInputBreak();
         }
@@ -99,20 +100,24 @@ class InputSequence {
     }
 
     #handleInputBreak() {
-        if (this.onInputBreak)
-            this.onInputBreak();
+        if (this.onInputBreak) this.onInputBreak();
         this.#inputStage = 0;
     }
 
     getAllInputs() {
-        return this.inputs.map(e => InputController.createBoundInputKey(e));
+        return this.inputs.map((e) => InputController.createBoundInputKey(e));
     }
 }
 
 class KeyEvent {
     // type e.g. keydown, hold, keyup
     // action e.g. a function to call when the key event happens
-    constructor(code, type = 'keydown', action = null, timeout = standardInputTimeout) {
+    constructor(
+        code,
+        type = 'keydown',
+        action = null,
+        timeout = standardInputTimeout
+    ) {
         this.code = code;
         this.type = type;
         this.action = action;
@@ -120,8 +125,4 @@ class KeyEvent {
     }
 }
 
-export {
-    InputController,
-    InputSequence,
-    KeyEvent
-}
+export { InputController, InputSequence, KeyEvent };
